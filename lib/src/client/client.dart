aa35e7141ed120d1df5d7dad535359b6325715eb
Ivan <ivan@ivk1800.ru>
2022-07-09 22:14:52 +0400

    add docs

--- lib/src/client/client.dart
+++ lib/src/client/client.dart
@@ -5,15 +5,23 @@ import 'platform/web/web_platform.dart'
     if (dart.library.ffi) 'platform/io/io_platform.dart';
 
 abstract class Client {
+  /// Creates new Client.
   factory Client.create() => ClientImpl(platform: PlatformImpl());
 
+  /// Receives incoming updates.
   Stream<TdObject> get updates;
 
+  /// Client initiation. Must be called once after creation.
   Future<void> initialize();
 
+  /// Sends a request to the TDLib. Must be called after initialization.
   Future<T> send<T extends TdObject>(TdFunction function);
 
+  /// Synchronously executes a TDLib request. Only a few marked accordingly
+  /// requests can be executed synchronously. Must be called after
+  /// initialization.
   T execute<T extends TdObject>(TdFunction function);
 
+  /// Destroy client. Must be called once after initialization.
   void destroy();
 }
